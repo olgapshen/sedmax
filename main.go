54cfafee19070c80b522ed1b3662a32adc301ae0
Olga Pshenichnikova <ya.olgapshenichnikova@yandex.ru>
2020-10-03 00:41:16 +0300

    Writing works fine

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 const (
@@ -15,33 +16,14 @@ const (
 const (
 	HEADER_LENGTH           = 8
 	RESP_PERS_MREQ_D_LENGTH = 4
+	REQ_PERS_MREQ_MD_LENGTH = 5
 
 	CMD_PERSET_M_REG = 0x10
+	CMD_READ_HOL_REG = 0x03
 )
 
-func main() {
-	// Listen for incoming connections.
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
-	}
-	// Close the listener when the application closes.
-	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-	for {
-		// Listen for an incoming connection.
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
-		}
-		// Handle connections in a new goroutine.
-		go handleTCPRequest(conn)
-	}
-}
-
-var Storage []byte
+var Storage map[int16]int16
+var Timeouts map[int16]time.Time
 
 type PDUHeader struct {
 	txid int16
@@ -56,13 +38,34 @@ type RequestPresetMReg struct {
 	addr int16
 	rcnt int16
 	leng byte
-	data []byte
+	data []int16
+}
+
+type RequestReadHReg struct {
+	head PDUHeader
+	addr int16
+	rcnt int16
 }
 
 type ResponsePresetMReg struct {
 	head PDUHeader
 	addr int16
 	amnt int16
+}
+
+type ResponseReadHReg struct {
+	head PDUHeader
+	leng byte
+	data []int16
+}
+
+func joinBytes(buf []byte) int16 {
+	return int16(buf[0])<<8 | int16(buf[1])
+}
+
+func splitBytes(buf []byte, value int16) {
+	buf[0] = byte(value >> 8)
+	buf[1] = byte(value)
 }
 
 func sliceHeader(pkg []byte) []byte {
@@ -75,63 +78,92 @@ func sliceBody(pkg []byte, dataLength int16) []byte {
 
 func parseHeader(buf []byte) PDUHeader {
 	var reqHeader PDUHeader
-	reqHeader.txid = int16(buf[0])<<8 | int16(buf[1])
-	reqHeader.prid = int16(buf[2])<<8 | int16(buf[3])
-	reqHeader.leng = int16(buf[4])<<8 | int16(buf[5])
+	reqHeader.txid = joinBytes(buf[0:])
+	reqHeader.prid = joinBytes(buf[2:])
+	reqHeader.leng = joinBytes(buf[4:])
 	reqHeader.addr = buf[6]
 	reqHeader.metd = buf[7]
 
 	return reqHeader
 }
 
-func parsePersetMReg(buf []byte) RequestPresetMReg {
-	var command RequestPresetMReg
-	command.addr = int16(buf[0])<<8 | int16(buf[1])
-	command.rcnt = int16(buf[2])<<8 | int16(buf[3])
-	command.leng = buf[4]
-	command.data = buf[5 : command.leng+5]
-	return command
-}
-
 func serializeHeader(header PDUHeader) []byte {
 	buf := make([]byte, HEADER_LENGTH)
-	buf[0] = byte(header.txid >> 8)
-	buf[1] = byte(header.txid)
-	buf[2] = byte(header.prid >> 8)
-	buf[3] = byte(header.prid)
-	buf[4] = byte(header.leng >> 8)
-	buf[5] = byte(header.leng)
+	splitBytes(buf[0:], header.txid)
+	splitBytes(buf[2:], header.prid)
+	splitBytes(buf[4:], header.leng)
 	buf[6] = header.addr
 	buf[7] = header.metd
 
 	return buf
 }
 
+func parsePersetMReg(header PDUHeader, buf []byte) RequestPresetMReg {
+	var request RequestPresetMReg
+	request.head = header
+	request.addr = joinBytes(buf[0:])
+	request.rcnt = joinBytes(buf[2:])
+	request.leng = buf[4]
+	request.data = make([]int16, request.rcnt)
+	rawData := buf[REQ_PERS_MREQ_MD_LENGTH : request.leng+REQ_PERS_MREQ_MD_LENGTH]
+
+	var i int16
+	for i = 0; i < request.rcnt; i++ {
+		request.data[i] = joinBytes(rawData[i*2:])
+	}
+
+	return request
+}
+
 func serilizePersetMReg(resp ResponsePresetMReg) []byte {
 	buf := make([]byte, HEADER_LENGTH+RESP_PERS_MREQ_D_LENGTH)
 	copy(buf, serializeHeader(resp.head))
-	buf[8] = byte(resp.addr >> 8)
-	buf[9] = byte(resp.addr)
-	buf[10] = byte(resp.amnt >> 8)
-	buf[11] = byte(resp.amnt)
+	splitBytes(buf[8:], resp.addr)
+	splitBytes(buf[10:], resp.amnt)
 
+	return buf
+}
+
+func parseReadHReg(header PDUHeader, buf []byte) RequestReadHReg {
+	var request RequestReadHReg
+	request.head = header
+	request.addr = joinBytes(buf[0:])
+	request.rcnt = joinBytes(buf[2:])
+	return request
+}
+
+func serializeReadHReg(resp ResponseReadHReg) []byte {
+	buf := make([]byte, HEADER_LENGTH+resp.leng+1)
+	copy(buf, serializeHeader(resp.head))
+	//splitBytes(buf[8:], resp.leng)
+	//splitBytes(buf[10:], resp.amnt)
 	return buf
 }
 
 func handlePersetMReg(request RequestPresetMReg) ResponsePresetMReg {
 	var response ResponsePresetMReg
 	response.head = request.head
+
+	var i int16
+	for i = 0; i < request.rcnt; i++ {
+		addr := request.addr + i
+		Storage[addr] = request.data[i]
+		Timeouts[addr] = time.Now()
+	}
+
 	response.addr = request.addr
 	response.amnt = request.rcnt
 
 	return response
 }
 
+func handleReadHReg() {
+
+}
+
 func handleTCPRequest(conn net.Conn) {
-	// Make a buffer to hold incoming data.
 	bufReq := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
-	reqLen, err := conn.Read(bufReq)
+	_, err := conn.Read(bufReq)
 
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
@@ -143,36 +175,49 @@ func handleTCPRequest(conn net.Conn) {
 	reqBody := sliceBody(bufReq, reqHeader.leng)
 
 	var bufResp []byte
-	//arrResp := make([]byte, 12)
 
 	switch reqHeader.metd {
 	case CMD_PERSET_M_REG:
-		persetMReg := parsePersetMReg(reqBody)
-		persetMReg.head = reqHeader
-		//arrResp := make([]byte, 12)
-		//copy(arrResp, arrHeader)
+		persetMReg := parsePersetMReg(reqHeader, reqBody)
 		response := handlePersetMReg(persetMReg)
 		bufResp = serilizePersetMReg(response)
-
-		//fmt.Printf("%v\n", persetMReg)
+	case CMD_READ_HOL_REG:
+		persetMReg := parsePersetMReg(reqHeader, reqBody)
+		response := handlePersetMReg(persetMReg)
+		bufResp = serilizePersetMReg(response)
 	default:
 		fmt.Println("Unknown MODBus method")
 	}
 
-	for i := 0; i < reqLen; i++ {
-		//fmt.Printf("%x ", buf[i])
-	}
-
-	fmt.Println()
-
-	for i := 0; i < 12; i++ {
-		//fmt.Printf("%x ", arrResp[i])
-	}
-
-	fmt.Println()
-
 	conn.Write(bufResp)
 
-	// Close the connection when you're done with it.
+	fmt.Println(Storage)
+	fmt.Println(Timeouts)
+
 	conn.Close()
+}
+
+func main() {
+	Storage = make(map[int16]int16)
+	Timeouts = make(map[int16]time.Time)
+
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleTCPRequest(conn)
+	}
 }
