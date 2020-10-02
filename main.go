9a457c49d7605b5c4af8218359d237476af7c16c
Olga Pshenichnikova <ya.olgapshenichnikova@yandex.ru>
2020-10-02 23:22:59 +0300

    Return success on write

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ const (
 )
 
 const (
+	HEADER_LENGTH           = 8
+	RESP_PERS_MREQ_D_LENGTH = 4
+
 	CMD_PERSET_M_REG = 0x10
 )
 
@@ -34,7 +37,7 @@ func main() {
 			os.Exit(1)
 		}
 		// Handle connections in a new goroutine.
-		go handleRequest(conn)
+		go handleTCPRequest(conn)
 	}
 }
 
@@ -49,6 +52,7 @@ type PDUHeader struct {
 }
 
 type RequestPresetMReg struct {
+	head PDUHeader
 	addr int16
 	rcnt int16
 	leng byte
@@ -56,15 +60,24 @@ type RequestPresetMReg struct {
 }
 
 type ResponsePresetMReg struct {
+	head PDUHeader
 	addr int16
 	amnt int16
 }
 
+func sliceHeader(pkg []byte) []byte {
+	return pkg[0:HEADER_LENGTH]
+}
+
+func sliceBody(pkg []byte, dataLength int16) []byte {
+	return pkg[HEADER_LENGTH : dataLength+HEADER_LENGTH]
+}
+
 func parseHeader(buf []byte) PDUHeader {
 	var reqHeader PDUHeader
-	reqHeader.txid = int16(buf[0]<<8) | int16(buf[1])
-	reqHeader.prid = int16(buf[2]<<8) | int16(buf[3])
-	reqHeader.leng = int16(buf[4]<<8) | int16(buf[5])
+	reqHeader.txid = int16(buf[0])<<8 | int16(buf[1])
+	reqHeader.prid = int16(buf[2])<<8 | int16(buf[3])
+	reqHeader.leng = int16(buf[4])<<8 | int16(buf[5])
 	reqHeader.addr = buf[6]
 	reqHeader.metd = buf[7]
 
@@ -73,56 +86,81 @@ func parseHeader(buf []byte) PDUHeader {
 
 func parsePersetMReg(buf []byte) RequestPresetMReg {
 	var command RequestPresetMReg
-	command.addr = int16(buf[0]<<8) | int16(buf[1])
-	command.rcnt = int16(buf[2]<<8) | int16(buf[3])
+	command.addr = int16(buf[0])<<8 | int16(buf[1])
+	command.rcnt = int16(buf[2])<<8 | int16(buf[3])
 	command.leng = buf[4]
 	command.data = buf[5 : command.leng+5]
 	return command
 }
 
-// Handles incoming requests.
-func handleRequest(conn net.Conn) {
+func serializeHeader(header PDUHeader) []byte {
+	buf := make([]byte, HEADER_LENGTH)
+	buf[0] = byte(header.txid >> 8)
+	buf[1] = byte(header.txid)
+	buf[2] = byte(header.prid >> 8)
+	buf[3] = byte(header.prid)
+	buf[4] = byte(header.leng >> 8)
+	buf[5] = byte(header.leng)
+	buf[6] = header.addr
+	buf[7] = header.metd
+
+	return buf
+}
+
+func serilizePersetMReg(resp ResponsePresetMReg) []byte {
+	buf := make([]byte, HEADER_LENGTH+RESP_PERS_MREQ_D_LENGTH)
+	copy(buf, serializeHeader(resp.head))
+	buf[8] = byte(resp.addr >> 8)
+	buf[9] = byte(resp.addr)
+	buf[10] = byte(resp.amnt >> 8)
+	buf[11] = byte(resp.amnt)
+
+	return buf
+}
+
+func handlePersetMReg(request RequestPresetMReg) ResponsePresetMReg {
+	var response ResponsePresetMReg
+	response.head = request.head
+	response.addr = request.addr
+	response.amnt = request.rcnt
+
+	return response
+}
+
+func handleTCPRequest(conn net.Conn) {
 	// Make a buffer to hold incoming data.
-	buf := make([]byte, 1024)
+	bufReq := make([]byte, 1024)
 	// Read the incoming connection into the buffer.
-	reqLen, err := conn.Read(buf)
+	reqLen, err := conn.Read(bufReq)
 
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
+		return
 	}
 
-	arrHeader := buf[0:8]
-
-	//fmt.Println(arrHeader)
-	//fmt.Println(buf)
-
+	arrHeader := sliceHeader(bufReq)
 	reqHeader := parseHeader(arrHeader)
-	//fmt.Printf("%v\n", reqHeader)
-	//fmt.Println(buf)
+	reqBody := sliceBody(bufReq, reqHeader.leng)
 
-	arrResp := make([]byte, 12)
-
-	for i := 0; i < reqLen; i++ {
-		fmt.Printf("%x ", buf[i])
-	}
-	fmt.Println()
+	var bufResp []byte
+	//arrResp := make([]byte, 12)
 
 	switch reqHeader.metd {
 	case CMD_PERSET_M_REG:
-		persetMReg := parsePersetMReg(buf[8 : reqHeader.leng+8])
+		persetMReg := parsePersetMReg(reqBody)
+		persetMReg.head = reqHeader
 		//arrResp := make([]byte, 12)
-		copy(arrHeader, arrResp)
-		arrResp[8] = byte(persetMReg.addr >> 8)
-		arrResp[9] = byte(persetMReg.addr)
-		arrResp[10] = byte(5 >> 8)
-		arrResp[11] = byte(5)
+		//copy(arrResp, arrHeader)
+		response := handlePersetMReg(persetMReg)
+		bufResp = serilizePersetMReg(response)
+
 		//fmt.Printf("%v\n", persetMReg)
 	default:
 		fmt.Println("Unknown MODBus method")
 	}
 
 	for i := 0; i < reqLen; i++ {
-		fmt.Printf("%x ", buf[i])
+		//fmt.Printf("%x ", buf[i])
 	}
 
 	fmt.Println()
@@ -132,6 +170,8 @@ func handleRequest(conn net.Conn) {
 	}
 
 	fmt.Println()
+
+	conn.Write(bufResp)
 
 	// Close the connection when you're done with it.
 	conn.Close()
